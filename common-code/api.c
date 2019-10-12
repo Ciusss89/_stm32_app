38077ed0f5130a022403ca2ffcf45acf074492d0
Giuseppe Tipaldi <giuseppe.tipaldi@tanaza.com>
2019-10-12 22:33:10 +0200

    shared-lib: add buffer init/destroy tool

--- common-code/api.c
+++ common-code/api.c
@@ -1,1 +1,30 @@
 #include "api.h"
+
+void *_malloc(size_t size)
+{
+	void *pt = malloc(size ? size : 1);
+	if (pt == NULL)
+		abort();
+	return pt;
+}
+
+struct buffer *buffer_init(size_t size)
+{
+	struct buffer *p = NULL;
+
+	p = _malloc(sizeof(struct buffer));
+	p->data = _malloc(size);
+	p->lenght = size;
+
+	return p;
+}
+
+void buffer_clean(struct buffer *p)
+{
+	if (!p)
+		return;
+
+	free(p->data);
+	free(p);
+}
+
--- common-code/api.h
+++ common-code/api.h
@@ -1,9 +1,38 @@
 /*
- * Author (c) 201 Giuseppe Tipaldi
+ * Author (c) 2019 Giuseppe Tipaldi
  *
  */
 
 #ifndef API_H
 #define API_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+struct buffer {
+	char *data;
+	size_t lenght;
+};
+
+/* @_malloc: like malloc but check if a pointer is not null
+ *
+ */
+void *_malloc(size_t size);
+
+/* @buffer_init:
+ *
+ *  example:
+ *  struct buffer *buff;
+ *  ..
+ *  buff = buffer_init(64);
+ *  ..
+ *  buffer_clean(buff);
+ */
+struct buffer *buffer_init(size_t size);
+
+/* @buffer_clean: kill the buffer
+ *
+ */
+void buffer_clean(struct buffer *b);
 
 #endif
