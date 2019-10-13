7f5cc03730e384fe1557321c288c1240e475f447
Giuseppe Tipaldi <giuseppe.tipaldi@tanaza.com>
2019-10-13 21:46:12 +0200

    shared-lib: tools: add _system_panic helper
    Example of usage:
    -----------------------------
    if(err)
    	__system_panic();
    -----------------------------

--- common-code/tools.c
+++ common-code/tools.c
@@ -4,6 +4,40 @@ void *_malloc(size_t size)
 {
 	void *pt = malloc(size ? size : 1);
 	if (pt == NULL)
-		abort();
+		_system_panic();
 	return pt;
+}
+
+static void _ld2_pulse(void)
+{
+		gpio_toggle(GPIOA, GPIO5);
+
+		for (int i = 0; i < 200000; i++)
+			__asm__("nop");
+
+		gpio_toggle(GPIOA, GPIO5);
+}
+
+void _system_panic(void)
+{
+	/* Instead to inizialize and print en error on the stderr/stdout,
+	 * I use the led LD2 of the nuclo board as panic indicator.
+	 */
+
+	rcc_periph_clock_enable(RCC_GPIOA);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+
+	/* Turn off led */
+	gpio_clear(GPIOA, GPIO5);
+
+	while (1) {
+		_ld2_pulse();
+
+		for (int i = 0; i < 1000000; i++)
+			__asm__("nop");
+
+		_ld2_pulse();
+		for (int i = 0; i < 8000000; i++)
+			__asm__("nop");
+	}
 }
--- common-code/tools.h
+++ common-code/tools.h
@@ -9,9 +9,15 @@
 #include <stdlib.h>
 #include <stddef.h>
 
-/* @_malloc: like malloc but check if a pointer is not null
- *
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+/* @_malloc: like malloc but in case of NULL pointer it calls _system_panic.
  */
 void *_malloc(size_t size);
 
+/* @_system_panic: blink in a panic mode to indicate by the led LD2 that
+ * 		   semething goes bad
+ */
+void _system_panic(void);
 #endif
