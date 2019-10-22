6644fa60338a4a91967a2bc23ea96a30d625c2b0
Giuseppe Tipaldi <giuseppe.tipaldi@tanaza.com>
2019-10-22 16:14:06 +0200

    shared-lib: buffer: check if the allocation fails

--- common-code/buffer.c
+++ common-code/buffer.c
@@ -6,6 +6,9 @@ struct buffer *buffer_init(size_t size)
 	struct buffer *p = NULL;
 
 	p = _malloc(sizeof(struct buffer));
+	if (!p)
+		_system_panic();
+
 	p->data = _malloc(size);
 	p->lenght = size;
 
